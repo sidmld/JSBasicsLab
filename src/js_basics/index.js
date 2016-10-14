b6bac5e6fd6336debe843fd85746f435e86db7ef
sidmld <sidmld93@gmail.com>
2016-10-14 20:45:33 +0200

    un test de plus...

--- src/js_basics/index.js
+++ src/js_basics/index.js
@@ -1,31 +1,84 @@
 
 export const isNumberEven = (i) => {
   // i will be an integer.
+  var i;
   // Return true if it's even, and false if it isn't.
+   if(i%2===0)
+       return true;
+   else 
+       return false;
 };
 
 export const getFileExtension = (str) => {
   // str will be a string, but it may not have a file extension.
+    var str;
   // Return the file extension (with no period) if it has one, otherwise false
+   
+  if(str.indexOf('.')!==-1){
+     var str2=str.split('.');
+      return str2[1];
+   }
+  else 
+    return false ;
+   
 };
 
 export const longestString = (arr) => {
   // arr will be an array.
+  var arr;
   // Return the longest string in the array
+   var longest = 0;
+    var longestString = "";
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].length > longest && typeof arr[i] =="string") {
+        longest = arr[i].length;
+        longestString = arr[i];
+        }
+    }
+    return longestString;
 };
 
 export const reverseString = (str) => {
   // str will be an string
+   var str;
   // Return a new string who's characters are in the opposite order to str's.
+   var o = '';
+  for (var i = str.length - 1; i >= 0; i--)
+    o += str[i];
+  return o;
 };
 
 export const isPalindrome = (str) => {
   // str will be an string
+   var str;
   // Return true if it is a palindrome and false otherwise.
+ var palin = str.split("").reverse().join("");
+
+    if (palin === str){
+        return true;
+    } else {
+        return false;
+    }
   // It should be case insensitive and not consider space or punctuation.
 };
 
 export const nestedSum = (arr) => {
   // arr will be an array, containing integers, strings and/or arrays like itself
+  var arr;
   // Return the sum all the numbers you find, anywhere in the nest of arrays.
+   var sum = 0;
+  var sum1 = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === Math.round(arr[i])) { //check if its an integar
+      sum += arr[i];
+    }  
+    if (arr[i] instanceof Array) {
+      for (var n = 0; n < arr[i].length; n++) {
+        sum1 += arr[i][n];
+      }
+    }
+  }
+  return sum + sum1;
+
 };
